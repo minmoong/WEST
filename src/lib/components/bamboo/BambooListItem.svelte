6e6929ecc4783701debbecb3e2396badd4be7265
minmoong <kim774634@gmail.com>
2024-01-11 18:43:54 +0900

    타이틀 길이 넘어갈시 ... 으로 표시

--- src/lib/components/bamboo/BambooListItem.svelte
+++ src/lib/components/bamboo/BambooListItem.svelte
@@ -20,15 +20,15 @@
 <div
 	class="flex w-full rounded-2xl border p-3 transition-colors hover:bg-gray-100 dark:border-gray-600 dark:bg-gray-700 dark:hover:bg-gray-600"
 >
-	<div class="mx-3 flex w-10 flex-col items-center justify-center">
+	<div class="mx-3 flex w-10 shrink-0 flex-col items-center justify-center">
 		<div>
 			<CaretUpSolid size="xs" class="text-gray-400 dark:text-slate-300" />
 		</div>
 		<div>{postData.views}</div>
 	</div>
-	<div>
-		<div>{postData.title}</div>
-		<div class="flex">
+	<div class="flex grow flex-col overflow-hidden">
+		<div class="overflow-hidden overflow-ellipsis whitespace-nowrap">{postData.title}</div>
+		<div class="flex overflow-hidden whitespace-nowrap">
 			<div>{postData.category}</div>
 			<div
 				class="ml-2 before:mr-2 before:inline-block before:h-3 before:border before:border-slate-300 before:align-middle dark:before:border-slate-500"
@@ -36,7 +36,7 @@
 				{calculateElapsedTime(postData.createdAt)}
 			</div>
 			<div
-				class="ml-2 before:mr-2 before:inline-block before:h-3 before:border before:border-slate-300 before:align-middle dark:before:border-slate-500"
+				class="ml-2 overflow-hidden overflow-ellipsis before:mr-2 before:inline-block before:h-3 before:border before:border-slate-300 before:align-middle dark:before:border-slate-500"
 			>
 				{postData.author.username}
 			</div>
