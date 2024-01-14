9f5a2c9fc84a88ac41a0432f04e5af31df714cb8
minmoong <kim774634@gmail.com>
2024-01-14 22:02:46 +0900

    깔끔쓰

--- src/lib/stores/posts.ts
+++ src/lib/stores/posts.ts
@@ -7,30 +7,6 @@ type PostStoreData = {
 	isFullLoaded: boolean;
 };
 
-/**
- * 서버로부터 새 글(미리보기용 데이터)을 받아오고
- * 그 새 글과 더 받아올 글이 남아있는지 여부를 리턴합니다.
- */
-const fetchPosts = async (
-	prevValue: PostStoreData
-): Promise<{ newPosts: PostPreview[]; isFullLoaded: boolean }> => {
-	const body = {
-		pageNumber: prevValue.pageNumber
-	};
-	const init = {
-		method: 'POST',
-		body: JSON.stringify(body)
-	};
-
-	const res = await fetch('/api/bamboo/get-posts', init);
-	const data = await res.json();
-
-	const newPosts = data.posts.map((i: PostPreview) => ({ ...i, createdAt: new Date(i.createdAt) }));
-	const isFullLoaded = data.isFullLoaded;
-
-	return { newPosts, isFullLoaded };
-};
-
 export const posts = writable<PostStoreData>({
 	data: [],
 	pageNumber: 0,
@@ -59,4 +35,28 @@ export const resetPostsStore = async () => {
 		pageNumber: 0,
 		isFullLoaded: false
 	});
+};
+
+/**
+ * 서버로부터 새 글(미리보기용 데이터)을 받아오고
+ * 그 새 글과 더 받아올 글이 남아있는지 여부를 리턴합니다.
+ */
+const fetchPosts = async (
+	prevValue: PostStoreData
+): Promise<{ newPosts: PostPreview[]; isFullLoaded: boolean }> => {
+	const body = {
+		pageNumber: prevValue.pageNumber
+	};
+	const init = {
+		method: 'POST',
+		body: JSON.stringify(body)
+	};
+
+	const res = await fetch('/api/bamboo/get-posts', init);
+	const data = await res.json();
+
+	const newPosts = data.posts.map((i: PostPreview) => ({ ...i, createdAt: new Date(i.createdAt) }));
+	const isFullLoaded = data.isFullLoaded;
+
+	return { newPosts, isFullLoaded };
 };
