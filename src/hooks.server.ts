f293366f7e4c8b180eae5349c0ad1cf1177c6af2
minmoong <kim774634@gmail.com>
2024-05-04 01:11:16 +0900

    인증 실패시 쿠키 자동 삭제

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,4 +1,4 @@
-import { error, redirect } from '@sveltejs/kit';
+import { redirect } from '@sveltejs/kit';
 import { authUser } from '$lib/server/auth';
 
 export const handle = async ({ event, resolve }) => {
@@ -10,11 +10,7 @@ export const handle = async ({ event, resolve }) => {
 			event.locals.user = user;
 		}
 	} catch (err) {
-		console.error(err);
-		throw error(500, {
-			message:
-				'해당 계정을 인증하는 과정에 실패하였습니다. 문제가 지속될 경우 사이트 쿠키를 삭제하고 다시 접속해주세요.'
-		});
+		event.cookies.delete('Authorization', { path: '/' });
 	}
 
 	// 라우트 프로텍팅
