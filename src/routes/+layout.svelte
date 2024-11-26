3f59c1eeda33065af77d3b5bc18d6139ba24daab
minmoong <kim774634@gmail.com>
2024-11-27 02:27:25 +0900

    눈 효과 on

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,7 +4,7 @@
 	import { SvelteToast } from '@zerodevx/svelte-toast';
 	import { ProgressBar } from '@prgm/sveltekit-progress-bar';
 	import { injectAnalytics } from '@vercel/analytics/sveltekit';
-	import { AutumnLeaves } from '$lib/components';
+	import { Snowflake } from '$lib/components';
 	import { printLogo } from '$lib/utils/tools';
 
 	injectAnalytics();
@@ -16,9 +16,9 @@
 
 <SvelteToast />
 <ProgressBar class="text-primary-400 dark:text-primary-500" zIndex={60} />
-<AutumnLeaves />
+<!-- <AutumnLeaves /> -->
 <!-- <Cherryblossom /> -->
-<!-- <Snowflake /> -->
-<!-- 이거 켠다음 다크모드를 기본으로 바꿀 것 -->
+<Snowflake />
+<!-- 이거 켠다음 다크모드를 기본으로 바꿀 것 (화이트모드에서는 눈 안보임) -->
 
 <slot />
